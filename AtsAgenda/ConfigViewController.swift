7a5cf7295fbc36e48daa44b69146921a9ff05a6e
ADMIN <admin@ADMINs-Mac.local>
2017-05-26 12:22:13 -0700

    Estrutura de telas criado

---
+++ AtsAgenda/ConfigViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConfigViewController.swift
+//  AtsAgenda
+//
+//  Created by ADMIN on 26/05/17.
+//  Copyright © 2017 Aluindex corp. All rights reserved.
+//
+
+import Foundation
---
+++ ConfigViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConfigViewController.swift
+//  AtsAgenda
+//
+//  Created by ADMIN on 26/05/17.
+//  Copyright © 2017 Aluindex corp. All rights reserved.
+//
+
+import UIKit
+
+class ConfigViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
